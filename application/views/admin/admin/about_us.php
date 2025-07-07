2c1ba9153456bdecaead243825fb1500933af3cf
prince <princekushwahfzd2gmail.com>
2025-07-07 10:51:21 +0530

    update

--- application/views/admin/admin/about_us.php
+++ application/views/admin/admin/about_us.php
@@ -1,4 +1,3 @@
-
 <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/admin/texteditor/texteditor.css">
 
 <script src="<?= base_url(); ?>assets/admin/texteditor/bootstrap.bundle.js"></script>
@@ -10,35 +9,37 @@
 <script src="<?= base_url(); ?>assets/admin/plugins/summernote/summernote-bs4.min.js"></script>
 
 
+
 <script>
-  $(function () {
+$(function() {
     $('.textarea').summernote()
-  })
+})
 </script>
 
 
 <div class="row">
-    
+
     <div class="col-md-12">
-    
+
         <div class="main-card mb-12 card">
-            
+
             <div class="card-header">
                 <h5 class="card-title"><?= ucwords('Manage About Us'); ?></h5>
             </div>
 
-            <form class="needs-validation"  method="post" action="<?= base_url().'admin/about_us/update/1'; ?>" >
+            <form class="needs-validation" method="post" action="<?= base_url().'admin/about_us/update/1'; ?>">
 
                 <div class="card-body">
-                    
+
                     <div class="form-group">
-                
-                
-                        <textarea class="textarea" placeholder="Enter your About Us" name="about_us"style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"><?= $this->db->get_where('about_us', array('aid'=>1))->row()->about_us; ?></textarea>
-                
-                
+
+
+                        <textarea class="textarea" placeholder="Enter your About Us" name="about_us"
+                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"><?= $this->db->get_where('about_us', array('aid'=>1))->row()->about_us; ?></textarea>
+
+
                     </div>
-                    
+
                 </div>
 
                 <div class="card-footer">
@@ -53,6 +54,6 @@
 
     </div>
 
-</div><br/>
+</div><br />
 
 </div>
